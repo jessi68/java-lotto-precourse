32c805f6445de6a82db0c5bf84a05543f21c7c2a
(학생) 이서윤 (전기전자컴퓨터공학부) <lynn312@unist.ac.kr>
2020-12-17 16:48:31 +0900

    feat(LottoRepository) 로또 객체들을 담고있는 LottoRepository 클래스를 만들고 각 덩첨 등수를 가진 사용자의 로또 개수를 담고 있는 자료구조 값을 계산하는 기능 구현

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,15 @@
+package domain;
+
+import java.util.List;
+
+public class User {
+
+    private LottoRepository lottoRepository;
+    private Rank rank;
+
+    User(LottoRepository lottoRepository) {
+        this.lottoRepository = lottoRepository;
+    }
+
+    public void
+}
